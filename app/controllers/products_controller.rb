4dfc043ad9c0018b8905bd840c7ca4c9659e4933
Hiroki Kumagai <OCA2602IT0006@edu.oca.ac.jp>
2026-07-24 01:23:03 +0000

    aaaaaa

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
+# app/controllers/products_controller.rb
+
 class ProductsController < ApplicationController
-  # 新規登録画面
+  # 新規登録
   def new
     # 新しい商品を作成するための空のインスタンスを用意
     @product = Product.new
@@ -11,18 +13,23 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     # データをデータベースに保存する
     if @product.save
-      # 成功した場合、トップページへリダイレクト
-      redirect_to root_path
+      # 成功した場合、商品一覧へリダイレクト
+      redirect_to products_path
     else
       # 失敗した場合、入力内容を保持したままフォームを再表示
       render :new, status: :unprocessable_entity
     end
   end
 
+  # 商品一覧
+  def index
+    @products = Product.all
+  end
+
   private
 
-  # ストロングパラメータで、フォームから送信されたデータを許可する
-  def product_params
-    params.require(:product).permit(:name, :description, :price)
-  end
+    # ストロングパラメータで、フォームから送信されたデータを許可する
+    def product_params
+      params.require(:product).permit(:name, :description, :price)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
+# app/config/routes.rb
+
 Rails.application.routes.draw do
+  # 新規商品
+  get 'products/new'
   post 'products', to: 'products#create'  # 登録
-   root to: "homes#top"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # 商品一覧
+  get 'products', to: 'products#index'
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # トップページ
+  root to: "homes#top"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # 省略
+
 end
