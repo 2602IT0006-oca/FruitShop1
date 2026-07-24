f85a99f2c2d2aec2319e812f1317b27cfc7b2763
Hiroki Kumagai <OCA2602IT0006@edu.oca.ac.jp>
2026-07-24 00:48:42 +0000

    aaaaaaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post 'products', to: 'products#create'  # 登録
    root to: "homes#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
